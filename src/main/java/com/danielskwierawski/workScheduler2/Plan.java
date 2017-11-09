a50027033f58dd22c8902e7816f366a74706dfb4
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-09 19:13:10 +0100

    third commit

--- src/main/java/com/danielskwierawski/workScheduler2/Plan.java
+++ src/main/java/com/danielskwierawski/workScheduler2/Plan.java
@@ -6,5 +6,13 @@ import java.util.List;
 public class Plan {
     private List<Worker> workerList = new ArrayList<>();
     public static final int defaultWorkTime = 8;
+
+    public void addWorker(Worker worker) {
+        workerList.add(worker);
+    }
+
+    public List<Worker> getAllWorkers() {
+        return workerList;
+    }
 }
 
--- src/main/test/com/danielskwierawski/workScheduler2/PlanTest.java
+++ src/main/test/com/danielskwierawski/workScheduler2/PlanTest.java
@@ -1,7 +1,30 @@
 package com.danielskwierawski.workScheduler2;
 
-import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlanTest {
 
+    Plan sut;
+
+    @Test
+    public void name() throws Exception {
+        // given
+        sut = new Plan();
+        Worker worker1 = new Worker("Artur", "Kowalski");
+        Worker worker2 = new Worker("Zbigniew", "Wisniewski");
+        Worker worker3 = new Worker("Mariusz", "Kowalski");
+        List<Worker> workerList = new ArrayList<>();
+        workerList.add(worker1);
+        workerList.add(worker2);
+        // when
+        sut.addWorker(worker1);
+        sut.addWorker(worker2);
+        // then
+        assertThat(sut.getAllWorkers()).containsExactly(worker1,worker2);
+    }
 }
