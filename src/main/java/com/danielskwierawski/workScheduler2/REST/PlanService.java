4ce86e8acd7ce67994d3341dd0d6b9af3ec280a2
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-15 14:43:43 +0100

    simple REST GET

---
+++ src/main/java/com/danielskwierawski/workScheduler2/REST/PlanService.java
@@ -0,0 +1,20 @@
+package com.danielskwierawski.workScheduler2.REST;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class PlanService {
+
+    @GET
+    @Path("/hello/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sayHello(@PathParam("name") String name) {
+        return Response.ok(name).build();
+    }
+
+}
