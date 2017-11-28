286bf5b3fe693cc5eb6fef82853a40a25828cb52
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-28 22:41:49 +0100

    01 > 1

--- src/test/java/com/danielskwierawski/workScheduler2/NumberOfDaysTest.java
+++ src/test/java/com/danielskwierawski/workScheduler2/NumberOfDaysTest.java
@@ -11,21 +11,21 @@ public class NumberOfDaysTest {
     @Test
     public void getNumberOfDaysInGivenMonth() throws Exception {
         // given
-        LocalDate January = LocalDate.of(2017, 1, 01);
-        LocalDate February = LocalDate.of(2017, 2, 01);
-        LocalDate March = LocalDate.of(2017, 3, 01);
-        LocalDate April = LocalDate.of(2017, 4, 01);
-        LocalDate May = LocalDate.of(2017, 5, 01);
-        LocalDate June = LocalDate.of(2017, 6, 01);
-        LocalDate July = LocalDate.of(2017, 7, 01);
-        LocalDate August = LocalDate.of(2017, 8, 01);
-        LocalDate September = LocalDate.of(2017, 9, 01);
-        LocalDate October = LocalDate.of(2017, 10, 01);
-        LocalDate November = LocalDate.of(2017, 11, 01);
-        LocalDate December = LocalDate.of(2017, 12, 01);
-        LocalDate FebruaryCommonYear = LocalDate.of(2018, 2, 01);
-        LocalDate FebruaryCommonYear2 = LocalDate.of(2019, 2, 01);
-        LocalDate FebruaryLeapYear = LocalDate.of(2020, 2, 01);
+        LocalDate January = LocalDate.of(2017, 1, 1);
+        LocalDate February = LocalDate.of(2017, 2, 1);
+        LocalDate March = LocalDate.of(2017, 3, 1);
+        LocalDate April = LocalDate.of(2017, 4, 1);
+        LocalDate May = LocalDate.of(2017, 5, 1);
+        LocalDate June = LocalDate.of(2017, 6, 1);
+        LocalDate July = LocalDate.of(2017, 7, 1);
+        LocalDate August = LocalDate.of(2017, 8, 1);
+        LocalDate September = LocalDate.of(2017, 9, 1);
+        LocalDate October = LocalDate.of(2017, 10, 1);
+        LocalDate November = LocalDate.of(2017, 11, 1);
+        LocalDate December = LocalDate.of(2017, 12, 1);
+        LocalDate FebruaryCommonYear = LocalDate.of(2018, 2, 1);
+        LocalDate FebruaryCommonYear2 = LocalDate.of(2019, 2, 1);
+        LocalDate FebruaryLeapYear = LocalDate.of(2020, 2, 1);
         NumberOfDays sut = new NumberOfDays();
         // when
         Integer resultJanuary = sut.getNumberOfDays(January);
