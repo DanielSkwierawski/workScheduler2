08199b6b71b268ab1f9e37b923a25961e8b68911
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-30 13:58:08 +0100

    reorder tests

--- src/test/java/com/danielskwierawski/workScheduler2/REST/PlanServiceTest.java
+++ src/test/java/com/danielskwierawski/workScheduler2/REST/PlanServiceTest.java
@@ -9,11 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PlanServiceTest {
 
     @Test
-    public void checkGETWorkers() throws Exception {
+    public void checkGETWorkersWhenEmpty() throws Exception {
         // given
         delete("/workScheduler2/workers");
-        get("/workScheduler2/addInitial");
-        String expectedJson = "[{\"name\":\"Daniel\",\"surname\":\"Kowalski\",\"dayMap\":[]},{\"name\":\"Zbigniew\",\"surname\":\"Wisniewski\",\"dayMap\":[[\"2017-01-03\",{\"start\":null,\"end\":null}],[\"2017-01-02\",{\"start\":null,\"end\":null}],[\"2017-01-01\",{\"start\":null,\"end\":null}]]},{\"name\":\"Krzysztof\",\"surname\":\"Pienkowski\",\"dayMap\":[[\"2017-01-03\",{\"start\":null,\"end\":null}],[\"2017-01-02\",{\"start\":14,\"end\":22}],[\"2017-01-01\",{\"start\":6,\"end\":14}]]}]";
+        String expectedJson = "";
         // when
         String json = get("/workScheduler2/workers").asString();
         // then
@@ -21,10 +20,11 @@ public class PlanServiceTest {
     }
 
     @Test
-    public void checkGETWorkersWhenEmpty() throws Exception {
+    public void checkGETWorkers() throws Exception {
         // given
         delete("/workScheduler2/workers");
-        String expectedJson = "";
+        get("/workScheduler2/addInitial");
+        String expectedJson = "[{\"name\":\"Daniel\",\"surname\":\"Kowalski\",\"dayMap\":[]},{\"name\":\"Zbigniew\",\"surname\":\"Wisniewski\",\"dayMap\":[[\"2017-01-03\",{\"start\":null,\"end\":null}],[\"2017-01-02\",{\"start\":null,\"end\":null}],[\"2017-01-01\",{\"start\":null,\"end\":null}]]},{\"name\":\"Krzysztof\",\"surname\":\"Pienkowski\",\"dayMap\":[[\"2017-01-03\",{\"start\":null,\"end\":null}],[\"2017-01-02\",{\"start\":14,\"end\":22}],[\"2017-01-01\",{\"start\":6,\"end\":14}]]}]";
         // when
         String json = get("/workScheduler2/workers").asString();
         // then
