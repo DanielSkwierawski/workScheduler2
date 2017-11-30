6115afd7dc4e51f71cc11102e72cd7605b738171
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-30 14:13:38 +0100

    GET Worker Query Param tests

--- src/test/java/com/danielskwierawski/workScheduler2/REST/PlanServiceTest.java
+++ src/test/java/com/danielskwierawski/workScheduler2/REST/PlanServiceTest.java
@@ -59,4 +59,33 @@ public class PlanServiceTest {
         assertThat(jsonZbigniewWisniewski).isEqualTo(expectedJsonZbigniewWisniewski);
         assertThat(jsonKrzysztofPienkowski).isEqualTo(expectedJsonKrzysztofPienkowski);
     }
+
+    @Test
+    public void checkGETWorkerQueryParamWhenEmpty() throws Exception {
+        // given
+        delete("/workScheduler2/workers");
+        String expectedJson = "";
+        // when
+        String json = get("/workScheduler2/worker?name=Daniel&surname=Kowalski").asString();
+        // then
+        assertThat(json).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void checkGETWorkerQueryParam() throws Exception {
+        // given
+        delete("/workScheduler2/workers");
+        get("/workScheduler2/addInitial");
+        String expectedJsonDanielKowalski = "{\"name\":\"Daniel\",\"surname\":\"Kowalski\",\"dayMap\":[]}";
+        String expectedJsonZbigniewWisniewski = "{\"name\":\"Zbigniew\",\"surname\":\"Wisniewski\",\"dayMap\":[[\"2017-01-03\",{\"start\":null,\"end\":null}],[\"2017-01-02\",{\"start\":null,\"end\":null}],[\"2017-01-01\",{\"start\":null,\"end\":null}]]}";
+        String expectedJsonKrzysztofPienkowski = "{\"name\":\"Krzysztof\",\"surname\":\"Pienkowski\",\"dayMap\":[[\"2017-01-03\",{\"start\":null,\"end\":null}],[\"2017-01-02\",{\"start\":14,\"end\":22}],[\"2017-01-01\",{\"start\":6,\"end\":14}]]}";
+        // when
+        String jsonDanielKowalski = get("/workScheduler2/worker?name=Daniel&surname=Kowalski").asString();
+        String jsonZbigniewWisniewski = get("/workScheduler2/worker?name=Zbigniew&surname=Wisniewski").asString();
+        String jsonKrzysztofPienkowski = get("/workScheduler2/worker?name=Krzysztof&surname=Pienkowski").asString();
+        // then
+        assertThat(jsonDanielKowalski).isEqualTo(expectedJsonDanielKowalski);
+        assertThat(jsonZbigniewWisniewski).isEqualTo(expectedJsonZbigniewWisniewski);
+        assertThat(jsonKrzysztofPienkowski).isEqualTo(expectedJsonKrzysztofPienkowski);
+    }
 }
