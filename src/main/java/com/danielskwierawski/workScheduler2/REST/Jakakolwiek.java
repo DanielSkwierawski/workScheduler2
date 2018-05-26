37873d9004808020309ea787c633da40fe0eb5da
Daniel Modrzejewski <danio1118m@gmail.com>
2018-05-26 20:10:36 +0200

    part2

--- src/main/java/com/danielskwierawski/workScheduler2/REST/Jakakolwiek.java
+++ src/main/java/com/danielskwierawski/workScheduler2/REST/Jakakolwiek.java
@@ -1,4 +1,6 @@
 package com.danielskwierawski.workScheduler2.REST;
 
 public class Jakakolwiek {
+    //pierwsza zmiana
+    //druga zmiana
 }
--- src/test/java/com/danielskwierawski/workScheduler2/REST/JakakolwiekTest.java
+++ src/test/java/com/danielskwierawski/workScheduler2/REST/JakakolwiekTest.java
@@ -1,4 +1,6 @@
 package com.danielskwierawski.workScheduler2.REST;
 
 public class JakakolwiekTest {
+    //pierwsza zmiana w 2 pliku
+    //druga zmiana w 2 pliku
 }
